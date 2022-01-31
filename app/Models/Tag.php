92516967334419cb8a983d4aab1b21a275d62e26
Gleydson Silva <ghudson.ti@gmail.com>
2022-01-31 15:26:51 +0100

    Add our own Tag Model and change the seeders and factories

---
+++ app/Models/Tag.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+use Spatie\Tags\HasTags;
+
+class Tag extends Model
+{
+    use HasFactory;
+    use HasTags;
+
+    protected $guarded =[];
+
+    public static function getTagClassName(): string
+    {
+        return Tag::class;
+    }
+
+    public function tags(): MorphToMany
+    {
+        return $this
+            ->morphToMany(self::getTagClassName(), 'taggable', 'taggables', null, 'tag_id')
+            ->orderBy('order_column');
+    }
+}
